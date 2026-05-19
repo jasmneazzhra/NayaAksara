b34a88ce8b5d038ffa6dd683cd07f47998e54424
zzrftixx <andragiri.opy@gmail.com>
2026-05-20 00:51:22 +0700

    submitQuestImage

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,17 @@
-const   BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3000';
 
 export async function fetchData(endpoint) {
     const response = await fetch(`${BASE_URL}${endpoint}`);
+    return response.json();
+}
+export async function submitQuestImage(file) {
+    const formData = new FormData();
+    formData.append("image", file);
+
+    const response = await fetch(`${BASE_URL}/submit-quest`, {
+        method: "POST",
+        body: formData,
+    });
+
     return response.json();
 }
