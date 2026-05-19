c57d1897c7574d054fa02f40cd6334278b85d5ee
zzrftixx <andragiri.opy@gmail.com>
2026-05-19 23:12:59 +0700

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const questRoutes = require("./routes/questRoutes");
